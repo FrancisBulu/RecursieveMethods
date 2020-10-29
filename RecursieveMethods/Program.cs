fdcc415ffe3e3e94dcb169696e6107e7222c7e17
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-29 10:34:19 +0100

    Add project files.

---
+++ RecursieveMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursieveMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int m = 9;
+            int sum = CalculateSumRecursive(n, m);
+            Console.WriteLine($"The sumof given range is {sum}");
+        }
+        static int CalculateSumRecursive(int n, int m)
+        {
+            int sum = n;
+            if (n<m)
+            {
+                n++;
+                return sum += CalculateSumRecursive(n, m);
+
+            }
+            return sum;
+
+        }
+    }
+}
